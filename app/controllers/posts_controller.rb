a930aa7d819e808b8d9dd5df4e2fd6fcde5b6ed5
github username <github email address>
2018-04-11 16:07:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,32 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new
+  end
 
   def edit
     @post = Post.find(params[:id])
   end
 
-  def update
+  def update # define valid path & invalid path
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+      @post.update(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end
   end
+
+ def create # define valid path & invalid path
+    @post = Post.create({title: params[:title], content: params[:content], category: params[:category]})
+   if @post.valid? 
+    @post.save
+    redirect_to post_path(@post)
+    else 
+      render :new
+    end
+  end 
 
   private
 
